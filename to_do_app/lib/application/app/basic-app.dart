399a2e62c75c564546a03f5b1097242de5d82aaf
Cristina Cavaleri <cavaleri.cristina@outlook.be>
2023-12-13 17:36:21 +0100

    Application Architecture

---
+++ to_do_app/lib/application/app/basic-app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class BasicApp extends StatelessWidget{
+  const BasicApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+
+
+}
